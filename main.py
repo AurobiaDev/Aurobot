f2848dc37cc5fc218c6bda3d77d1c74bd7b73812
IzukiKa <isaacjroberts0505@gmail.com>
2022-11-13 19:10:44 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+
+from dotenv import dotenv_values
+
+TOKEN = dotenv_values(".env")["TOKEN"]
+
+class AurobotClient(discord.Client):
+    async def on_ready(self):
+        print(f"Logged on as {self.user}!")
+
+    async def on_message(self, message):
+        print(f"Message from {message.author}: {message.content}")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = AurobotClient(intents=intents)
+client.run(TOKEN, log_handler=None)
